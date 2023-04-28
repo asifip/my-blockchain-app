acd86d3155d7eaf4ba87bc5630d34c3db307aef5
asifip <apate277@my.centennialcollege.ca>
2023-04-28 03:17:06 +0000

    essential script to be run first

---
+++ essential.sh
@@ -0,0 +1,13 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash nvm install node
+
+npx create-react-app my-blockchain-app
+
+cd my-blockchain-app
+
+# npm install ethereumjs-util
+
+# npm install crypto-js
+
+npm install
+
+npm run build && npm start
